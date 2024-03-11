5c8e5b4a25aa9bab77711387ac8e9a956a3944e3
IRAM_ERNESTO_MARTINEZ_RUIZ <a.21308051280378@cbtis128.edu.mx>
2024-03-11 15:16:44 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:martinez0378/pantalla2_0378.dart';
+import 'package:martinez0378/pantalla1_0378.dart';
+import 'package:martinez0378/pantallainicial_0378.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiApp0378());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiApp0378 extends StatelessWidget {
+  const MiApp0378({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return MaterialApp(initialRoute: "/", routes: {
+      "/": (context) => PantallaInicial_0378(),
+      "/Pantall1_0378": (context) => Pantalla1_0378(),
+      "/Pantall2_0378": (context) => Pantalla2_0378(),
+    });
   }
 }
---
+++ lib/pantalla1_0378.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Pantalla1_0378 extends StatelessWidget {
+  const Pantalla1_0378({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pantalla1_0378"),
+          backgroundColor: Color(0xffc90b0b),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                child: Text("Martinez Ejemplo"),
+              )
+            ], //ninios
+          ),
+        ));
+  }
+}
